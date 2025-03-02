2798129710bd4911a7992f5ceda921727db261d3
Jose A Felix <jafb727@gmail.com>
2025-03-01 21:28:54 -0600

    adding an index file to /pages/app folder

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./pages/app/app.tsx";
+import App from "./pages/app";
 
 createRoot(document.getElementById("root")!).render(
    <StrictMode>
