d04b4ad21e5fe3f19b8cc95317d70edad1815c93
Jose A Felix <jafb727@gmail.com>
2025-03-01 21:26:50 -0600

    moving main app component to /pages/app folder

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./pages/app/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
    <StrictMode>
