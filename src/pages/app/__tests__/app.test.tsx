1fc77753eb4fe2ba273b8963bc0490c87a6b6c4d
Jose A Felix <jafb727@gmail.com>
2025-03-01 21:30:47 -0600

    adding basic app component unit testing file

---
+++ src/pages/app/__tests__/app.test.tsx
@@ -0,0 +1,12 @@
+/**
+ * @author Jose A Felix
+ * @name app.test
+ * @description Main application component unit testing file
+ */
+
+/** -------------------------------------------------------- */
+
+/** @import Components */
+import App from "..";
+
+/** -------------------------------------------------------- */
