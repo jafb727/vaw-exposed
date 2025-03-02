4d1f58bbc50fe2944fc97828d79d74069b637165
Jose A Felix <jafb727@gmail.com>
2025-03-01 22:00:16 -0600

    adding basic unit test cases to app component

--- src/pages/app/__tests__/app.test.tsx
+++ src/pages/app/__tests__/app.test.tsx
@@ -9,12 +9,35 @@
 /** @import Components */
 import App from "..";
 
+/** @import Utilities */
+import { renderComponent } from "../../../test/setupTests";
+
 /** -------------------------------------------------------- */
 
 describe("App", () => {
-   it("Renders ok", () => {});
+   it("Renders ok", () => {
+      /** Component context mounting */
+      const component = renderComponent(App);
 
-   it("Renders main component", () => {});
+      /** Main assertion */
+      expect(component).not.toBe(null);
+   });
 
-   it("Matches snapshot", () => {});
+   it("Renders main component", () => {
+      /** Component context mounting */
+      const { getByTestId } = renderComponent(App);
+
+      /** Main assertion */
+      const component = getByTestId("app-page");
+      expect(component).toBeInstanceOf(HTMLElement);
+   });
+
+   it("Matches snapshot", () => {
+      /** Component context mounting */
+      const { getByTestId } = renderComponent(App);
+
+      /** Main assertion */
+      const component = getByTestId("app-page");
+      expect(component).toMatchSnapshot();
+   });
 });
