41a7b40984fc192588b1fa936babf8bf12f69f9e
Jose A Felix <jafb727@gmail.com>
2025-03-01 18:45:45 -0600

    commenting /src root files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+/**
+ * @author Jose A Felix
+ * @name main
+ * @description Application entry file
+ */
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+/** -------------------------------------------------------- */
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+   <StrictMode>
+      <App />
+   </StrictMode>
+);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
+/**
+ * @author Jose A Felix
+ * @name vite.env.d
+ * @description Vite environment types configuration file
+ */
+
+/** -------------------------------------------------------- */
+
 /// <reference types="vite/client" />
